045243cf4ef1ba40b6127487af8a971505489411
manci <gmancillapaz@gmail.com>
2021-02-23 17:15:27 -0500

    creando scrollview horizontal con imagenes

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import {
   StyleSheet,
   View,
-  Image
+  Image,
+  Text,
+  ScrollView,
 } from 'react-native';
 
 
@@ -16,6 +18,44 @@ const App = () => {
           style={styles.banner}
           source={ require('./assets/img/bg.jpg')}
         />  
+      </View>
+
+      <View style={styles.contenedor}>
+        <Text style={styles.titulo}>Que hacer en Paris</Text>  
+        <ScrollView
+          horizontal
+        >
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={ require('./assets/img/actividad1.jpg')}
+            />  
+          </View>
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={ require('./assets/img/actividad2.jpg')}
+            />  
+          </View>
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={ require('./assets/img/actividad3.jpg')}
+            />  
+          </View>
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={ require('./assets/img/actividad4.jpg')}
+            />  
+          </View>
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={ require('./assets/img/actividad5.jpg')}
+            />  
+          </View>
+        </ScrollView>
       </View> 
      </View>
     </>
@@ -26,6 +66,19 @@ const styles = StyleSheet.create({
   banner: {
     height: 250,
     flex: 1,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
   }
 });
 
